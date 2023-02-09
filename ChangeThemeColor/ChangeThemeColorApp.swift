b3db4633ade657dfcc6d1b3d3b23ae61446c9f43
Judy Yu <judy.yuqiuyi@gmail.com>
2023-02-08 20:01:41 -0500

    Initial Commit

---
+++ ChangeThemeColor/ChangeThemeColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeThemeColorApp.swift
+//  ChangeThemeColor
+//
+//  Created by Judy YU on 2023-02-08.
+//
+
+import SwiftUI
+
+@main
+struct ChangeThemeColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
